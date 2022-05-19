307a3a28f99810e75fe52e30a267d79a13cd8baa
ahsan-virani <ahsan.virani@gmail.com>
2022-05-19 21:05:47 +0200

    test workflows before running test

--- vegeta/workers_health.sh
+++ vegeta/workers_health.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-ulimit -n 1048576 # open files
-
+# path=.
+path=/home/ubuntu/vegeta
 url_postfix="/healthz"
+
+ulimit -n 1048576 # open files
 
 for worker_health_uri in "$@"
 do
@@ -16,4 +18,26 @@ do
     done
 done
 
+# check workflows
+
+tested="-1"
+echo "Checking workflow health"
+while IFS= read -r line; do
+    vals=($line)
+    if [ "$tested" == "${vals[0]}" ]; then
+        echo "skip"
+    else
+        echo "Checking workflow - ${vals[0]}"
+        status=`curl -s -o /dev/null -w "%{http_code}" $1/webhook/workflow-${vals[0]}`
+        while [ $status -ne 200 ]
+        do
+            echo "Workflow ${vals[0]} not ready yet..."
+            sleep 5
+            status=`curl -s -o /dev/null -w "%{http_code}" $1/webhook/workflow-${vals[0]}`
+        done
+        tested=${vals[0]}
+    fi
+done < $path/tests
+
+echo "Workflow(s) ready"
 echo "Worker(s) ready"
